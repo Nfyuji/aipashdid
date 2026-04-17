b84981bf2af4675b90a052e56c5c77b75703f45c
Nfyuji <134148566+Nfyuji@users.noreply.github.com>
2026-04-17 16:50:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'phishdetector-secret-key-2026'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'phishdetector.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///phishdetector.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
